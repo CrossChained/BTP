33ff5e6ca875ef0abf30ddd2be7b34e4c1fa8722
Oleksii Bespalov <alexb5dh@gmail.com>
2020-05-13 11:12:12 +0300

    Config update

--- src/CrossChained.BTP.BitIndex.Client/BitIndexApiConfig.cs
+++ src/CrossChained.BTP.BitIndex.Client/BitIndexApiConfig.cs
@@ -1,9 +1,16 @@
-﻿namespace CrossChained.BTP.BitIndex.Client
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrossChained.BTP.BitIndex.Client
 {
     public class BitIndexApiConfig
     {
+        [Required]
         public string BaseUri { get; set; } = "https://api.bitindex.network/api/v3/";
+
+        [Required]
         public string ApiKey { get; set; }
+
+        [Required]
         public string Network { get; set; } = "Main";
     }
 }
