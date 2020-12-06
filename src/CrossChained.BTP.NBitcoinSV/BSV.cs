87f1c58e482039cf2a8c746708eda7aaf1a5838b
Oleksii Bespalov <alexb5dh@gmail.com>
2020-12-07 01:36:16 +0200

    NBitcoin update

--- src/CrossChained.BTP.NBitcoinSV/BSV.cs
+++ src/CrossChained.BTP.NBitcoinSV/BSV.cs
@@ -3,24 +3,22 @@ using NBitcoin.Protocol;
 
 namespace CrossChained.BTP.NBitcoinSV
 {
-	public class BSVConsensusFactory : ConsensusFactory
-	{
-		private BSVConsensusFactory()
-		{
+    public class BSVConsensusFactory: ConsensusFactory
+    {
+        private BSVConsensusFactory() { }
+        public static BSVConsensusFactory Instance { get; } = new BSVConsensusFactory();
 
-		}
-		public static BSVConsensusFactory Instance { get; } = new BSVConsensusFactory();
+        public override ProtocolCapabilities GetProtocolCapabilities(uint protocolVersion)
+        {
+            var capabilities = base.GetProtocolCapabilities(protocolVersion);
+            capabilities.SupportWitness = false;
+            return capabilities;
+        }
 
-		public override ProtocolCapabilities GetProtocolCapabilities(uint protocolVersion)
-		{
-			var capabilities = base.GetProtocolCapabilities(protocolVersion);
-			capabilities.SupportWitness = false;
-			return capabilities;
-		}
+        public override Transaction CreateTransaction() =>
+            new NBitcoin.Altcoins.ForkIdTransaction(0x00, false, this);
 
-		public override Transaction CreateTransaction()
-		{
-			return new NBitcoin.Altcoins.ForkIdTransaction(0x00, false, this);
-		}
-	}
+        public TransactionBuilder CreateTransactionBuilder(Network network) =>
+            network.CreateTransactionBuilder().ContinueToBuild(CreateTransaction());
+    }
 }
